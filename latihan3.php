38fc5e7a904fa515045aa0f5d4274a9f7d1ed667
muhammadrafie390-cloud <muhammadrafie390@gmail.com>
2026-09-10 13:40:45 +0700

    Add files via upload

---
+++ latihan3.php
@@ -0,0 +1,15 @@
+<?php
+$angka1 = 15;
+$angka2 = 5;
+
+$penjumlahan = $angka1 + $angka2;
+$pengurangan = $angka1 - $angka2;
+$perkalian   = $angka1 * $angka2;
+$pembagian   = $angka1 / $angka2;
+
+echo "<h2>Hasil Perhitungan:</h2>";
+echo "15 + 5 = " . $penjumlahan . "<br>";
+echo "15 - 5 = " . $pengurangan . "<br>";
+echo "15 * 5 = " . $perkalian . "<br>";
+echo "15 / 5 = " . $pembagian . "<br>";
+?>
