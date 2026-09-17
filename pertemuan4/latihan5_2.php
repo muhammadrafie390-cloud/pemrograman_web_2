19b969eeec35a43765c6e17a9794b1117c1a563b
muhammadrafie390-cloud <muhammadrafie390@gmail.com>
2026-09-17 19:38:53 +0700

    Add files via upload

---
+++ pertemuan4/latihan5_2.php
@@ -0,0 +1,10 @@
+<?php
+$angka = 12;
+echo "<pre>";
+echo "Tabel Perkalian\n";
+echo "================\n";
+for ($n = 15; $n <= 45; $n += 2) {
+    echo "$angka * $n = " . ($angka * $n) . "\n";
+}
+echo "</pre>";
+?>
